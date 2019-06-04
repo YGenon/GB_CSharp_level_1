b3a294945f2bdb2c1f2b64e46b72bfad758231d0
YGenon <genonster@gmail.com>
2019-06-04 23:06:03 +0300

    решено 4 задачи

--- ConsoleAppLesson2/ConsoleAppLesson2/Program.cs
+++ ConsoleAppLesson2/ConsoleAppLesson2/Program.cs
@@ -7,22 +7,48 @@ using System.Threading.Tasks;
 namespace ConsoleAppLesson2
 {
     class Program
-    {
+    {   
+        // Ященко Геннадий
+
+        /// <summary>
+        /// Проверка введенного логина и пароля
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="passwd"></param>
+        /// <returns></returns>
         static Boolean Authorization(string login, string passwd)
         {
             if (login == "root" && passwd == "GeekBrains")
             {
+                Console.WriteLine(" \n Вы авторизованы в системе! \n");
                 return true;
             }
             else
             {
+                Console.WriteLine("\n Логин или пароль не верен! \n");
                 return false;
             }
         }
 
+        /// <summary>
+        /// Подсчет суммы цифр в многозначном числе
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static int SummaNumber(string text)
+        {
+            int summa = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                summa += Convert.ToInt32(text.Substring(i, 1));
+            }
+            return summa;
+        }
+
         static void Main(string[] args)
         {
-            // Ященко Геннадий
+            
 
             #region 1 задача
             #region Условие задачи:
@@ -31,25 +57,25 @@ namespace ConsoleAppLesson2
             //Используя метод проверки логина и пароля, написать программу: пользователь вводит  логин и  пароль,  программа пропускает  его дальше  или не пропускает.С помощью цикла do while ограничить ввод пароля тремя попытками.
             #endregion
 
-            Console.Clear();
-            Console.WriteLine("1 задача \n");
-
-            Console.Write("Введите логин: ");
-            string login = Console.ReadLine();
-
-            Console.Write("Введите пароль: ");
-            string passwd = Console.ReadLine();
-
-            if (Authorization(login, passwd))
-            {
-                Console.WriteLine("Вы авторизованы в системе!");
-                
-            }
-            else
-            {
-                Console.WriteLine("Логин или пароль не верны. Повторите ввод!");
-            }
             
+            Console.WriteLine("1 задача - Проверки логина и пароля \n");
+
+            int count = 0;
+            do
+            {
+                Console.Write("Введите логин: ");
+                string login = Console.ReadLine();
+
+                Console.Write("Введите пароль: ");
+                string passwd = Console.ReadLine();
+
+                if (Authorization(login, passwd)) break;
+                count++;
+               // Console.Clear();
+
+            } while (count < 3);
+
+            if (count == 3) Console.WriteLine("Количество попыток ввода исчерпано ! \n");
             Console.ReadKey();
 
             #endregion
@@ -62,7 +88,7 @@ namespace ConsoleAppLesson2
             #endregion
 
             Console.WriteLine();
-            Console.WriteLine("2 задача \n");
+            Console.WriteLine("2 задача - Возвращение минимального числа из трёх введенных чисел \n");
 
             Console.Write("Введите первое число: ");
             int firstNumber = Convert.ToInt32(Console.ReadLine());
@@ -75,18 +101,23 @@ namespace ConsoleAppLesson2
 
             Console.WriteLine();
 
-            string rezult = " - минимальное число";
+            string rezult = "Минимальное введеное число - ";
 
             if (firstNumber < secondNumber && firstNumber < thirdNumber)
             {
-                Console.Write(firstNumber + rezult);
+                Console.WriteLine(rezult + firstNumber + "\n");                
             }
-            else
-            {
-                Console.WriteLine((secondNumber < thirdNumber) ? (secondNumber + rezult) : (thirdNumber + rezult));
+            else { 
+
+                if (secondNumber < thirdNumber)
+                {
+                    Console.WriteLine(rezult + secondNumber + "\n");
+                }
+                else
+                {
+                    Console.WriteLine(rezult + thirdNumber + "\n");
+                }
             }
-
-
             Console.ReadKey();
 
             #endregion
@@ -99,7 +130,7 @@ namespace ConsoleAppLesson2
             #endregion
 
             Console.WriteLine();
-            Console.WriteLine("3 задача \n");
+            Console.WriteLine("3 задача - Ввод чисел и подсчет суммы всех нечетных, положительных чисел\n");
             Console.WriteLine("Для завершения программы введите цифру - 0 \n");
 
             int number;
@@ -116,12 +147,11 @@ namespace ConsoleAppLesson2
 
                 if (number == 0)
                 {
-                    Console.WriteLine("Сумма все нечетных, положительных чисел равна - " + odd);
+                    Console.WriteLine("Сумма все нечетных, положительных чисел равна - " + odd +"\n \n");
                     break;
                 }
 
             } while (number != 0);
-
 
             Console.ReadKey();
 
@@ -135,24 +165,16 @@ namespace ConsoleAppLesson2
             #endregion
 
             Console.Clear();
-            Console.WriteLine("4 задача \n");
+            Console.WriteLine("4 задача - Подсчет количества цифр числа\n");
 
             Console.Write("Введите многозначное число: ");
-            string text = Console.ReadLine();
+            string text = Console.ReadLine();           
 
-            int summa = 0;
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                summa += Convert.ToInt32(text.Substring(i, 1));
-            }
-
-            Console.WriteLine("Сумма цифр введенного числа равняется - " + summa);
+            Console.WriteLine("Сумма цифр введенного числа равняется - " + SummaNumber(text));
             Console.ReadKey();
 
             #endregion
-
-
         }
+
     }
 }
