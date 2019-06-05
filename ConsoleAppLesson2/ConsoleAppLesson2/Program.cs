312e0b8a1c8a11092dd3298671edf26039df9a01
YGenon <genonster@gmail.com>
2019-06-05 06:47:55 +0300

    решил 6 задачу но нужно оптимизировать

--- ConsoleAppLesson2/ConsoleAppLesson2/Program.cs
+++ ConsoleAppLesson2/ConsoleAppLesson2/Program.cs
@@ -35,20 +35,20 @@ namespace ConsoleAppLesson2
         /// </summary>
         /// <param name="text"></param>
         /// <returns></returns>
-        static int SummaNumber(string text)
+        static UInt32 SummaNumber(string text)
         {
-            int summa = 0;
+            uint summa = 0;
 
             for (int i = 0; i < text.Length; i++)
             {
-                summa += Convert.ToInt32(text.Substring(i, 1));
+                summa += Convert.ToUInt32(text.Substring(i, 1));
             }
             return summa;
         }
 
         static void Main(string[] args)
         {
-            
+
 
             #region 1 задача
             #region Условие задачи:
@@ -57,26 +57,26 @@ namespace ConsoleAppLesson2
             //Используя метод проверки логина и пароля, написать программу: пользователь вводит  логин и  пароль,  программа пропускает  его дальше  или не пропускает.С помощью цикла do while ограничить ввод пароля тремя попытками.
             #endregion
 
-            
-            Console.WriteLine("1 задача - Проверки логина и пароля \n");
 
-            int count = 0;
-            do
-            {
-                Console.Write("Введите логин: ");
-                string login = Console.ReadLine();
+            //Console.WriteLine("1 задача - Проверки логина и пароля \n");
 
-                Console.Write("Введите пароль: ");
-                string passwd = Console.ReadLine();
+            //int count = 0;
+            //do
+            //{
+            //    Console.Write("Введите логин: ");
+            //    string login = Console.ReadLine();
 
-                if (Authorization(login, passwd)) break;
-                count++;
-               // Console.Clear();
+            //    Console.Write("Введите пароль: ");
+            //    string passwd = Console.ReadLine();
 
-            } while (count < 3);
+            //    if (Authorization(login, passwd)) break;
+            //    count++;
+            //   // Console.Clear();
 
-            if (count == 3) Console.WriteLine("Количество попыток ввода исчерпано ! \n");
-            Console.ReadKey();
+            //} while (count < 3);
+
+            //if (count == 3) Console.WriteLine("Количество попыток ввода исчерпано ! \n");
+            //Console.ReadKey();
 
             #endregion
 
@@ -87,38 +87,38 @@ namespace ConsoleAppLesson2
 
             #endregion
 
-            Console.WriteLine();
-            Console.WriteLine("2 задача - Возвращение минимального числа из трёх введенных чисел \n");
+            //Console.WriteLine();
+            //Console.WriteLine("2 задача - Возвращение минимального числа из трёх введенных чисел \n");
 
-            Console.Write("Введите первое число: ");
-            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите первое число: ");
+            //int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Введите второе число: ");
-            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Введите третье число: ");
-            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите третье число: ");
+            //int thirdNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            string rezult = "Минимальное введеное число - ";
+            //string rezult = "Минимальное введеное число - ";
 
-            if (firstNumber < secondNumber && firstNumber < thirdNumber)
-            {
-                Console.WriteLine(rezult + firstNumber + "\n");                
-            }
-            else { 
+            //if (firstNumber < secondNumber && firstNumber < thirdNumber)
+            //{
+            //    Console.WriteLine(rezult + firstNumber + "\n");                
+            //}
+            //else { 
 
-                if (secondNumber < thirdNumber)
-                {
-                    Console.WriteLine(rezult + secondNumber + "\n");
-                }
-                else
-                {
-                    Console.WriteLine(rezult + thirdNumber + "\n");
-                }
-            }
-            Console.ReadKey();
+            //    if (secondNumber < thirdNumber)
+            //    {
+            //        Console.WriteLine(rezult + secondNumber + "\n");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(rezult + thirdNumber + "\n");
+            //    }
+            //}
+            //Console.ReadKey();
 
             #endregion
 
@@ -129,31 +129,31 @@ namespace ConsoleAppLesson2
 
             #endregion
 
-            Console.WriteLine();
-            Console.WriteLine("3 задача - Ввод чисел и подсчет суммы всех нечетных, положительных чисел\n");
-            Console.WriteLine("Для завершения программы введите цифру - 0 \n");
+            //Console.WriteLine();
+            //Console.WriteLine("3 задача - Ввод чисел и подсчет суммы всех нечетных, положительных чисел\n");
+            //Console.WriteLine("Для завершения программы введите цифру - 0 \n");
 
-            int number;
-            int odd = 0;
+            //int number;
+            //int odd = 0;
 
-            do
-            {
-                Console.Write("Введите целое число: ");
-                number = Convert.ToInt32(Console.ReadLine());
-                if (number % 2 > 0 && number > 0)
-                {
-                    odd += number;
-                }
+            //do
+            //{
+            //    Console.Write("Введите целое число: ");
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //    if (number % 2 > 0 && number > 0)
+            //    {
+            //        odd += number;
+            //    }
 
-                if (number == 0)
-                {
-                    Console.WriteLine("Сумма все нечетных, положительных чисел равна - " + odd +"\n \n");
-                    break;
-                }
+            //    if (number == 0)
+            //    {
+            //        Console.WriteLine("Сумма все нечетных, положительных чисел равна - " + odd +"\n \n");
+            //        break;
+            //    }
 
-            } while (number != 0);
+            //} while (number != 0);
 
-            Console.ReadKey();
+            //Console.ReadKey();
 
             #endregion
 
@@ -164,13 +164,43 @@ namespace ConsoleAppLesson2
 
             #endregion
 
-            Console.Clear();
-            Console.WriteLine("4 задача - Подсчет количества цифр числа\n");
+            //Console.WriteLine("4 задача - Подсчет количества цифр числа\n");
 
-            Console.Write("Введите многозначное число: ");
-            string text = Console.ReadLine();           
+            //Console.Write("Введите многозначное, положительное число: ");
+            //string text = Console.ReadLine();
 
-            Console.WriteLine("Сумма цифр введенного числа равняется - " + SummaNumber(text));
+            //Console.WriteLine("Сумма цифр введенного числа равняется - " + SummaNumber(text));
+            //Console.ReadKey();
+
+            #endregion
+
+            #region 5 задача
+            #region Условие задачи:
+
+            //*Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000.
+            //«Хорошим»  называется число, которое  делится на  ​сумму своих  цифр​.Реализовать подсчёт времени выполнения программы,
+            //используя структуру DateTime.
+
+
+            #endregion
+
+            UInt32 count = 0;
+
+            DateTime start = DateTime.Now;
+
+            for (UInt32 i = 1; i <= 1000000000; i++)
+            {
+                string b = Convert.ToString(i);
+                UInt32 a = SummaNumber(b);
+                if (i % a == 0) count++;
+                if(i == 100000 || i == 1000000 || i == 10000000  || i == 1000000000) Console.WriteLine(i);
+
+            }
+            Console.WriteLine(count);
+
+            DateTime end = DateTime.Now;
+            Console.WriteLine(end - start);
+
             Console.ReadKey();
 
             #endregion
