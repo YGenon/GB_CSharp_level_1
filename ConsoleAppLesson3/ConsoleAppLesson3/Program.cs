f34d83017240017031187836d9d6f03cac1c4c74
YGenon <genonster@gmail.com>
2019-06-06 18:56:29 +0300

    создал проект класса Fraction

--- ConsoleAppLesson3/ConsoleAppLesson3/Program.cs
+++ ConsoleAppLesson3/ConsoleAppLesson3/Program.cs
@@ -6,10 +6,81 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppLesson3
 {
+    // Геннадий Ященко 
+
+    // Задача №3
+
+    #region условие Задачи
+
+    //*Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел. 
+    //    Предусмотреть методы  сложения,  вычитания,  умножения и  деления дробей.  
+    //    Написать программу, демонстрирующую все разработанные элементы класса. 
+
+    //*Добавить свойства типа int для доступа к числителю и знаменателю;
+
+    //*Добавить свойство типа double только на чтение, чтобы получить десятичную дробь числа;
+
+    //**Добавить  проверку,  чтобы знаменатель  не равнялся  0. Выбрасывать  исключение ArgumentException("Знаменатель не может быть равен 0");
+
+    //***Добавить упрощение дробей. 
+
+    #endregion
+
+    /// <summary>
+    /// Операции с дробными числами
+    /// </summary>
+    class Fraction
+    {
+        private int numerator1;
+        private int denumerator1;
+
+        private int numerator2;
+        private int denumerator2;
+
+        /// <summary>
+        /// Сумма дробей
+        /// </summary>
+        public Summa()
+        {
+
+        }
+
+        /// <summary>
+        /// Разница дробей
+        /// </summary>
+        public Difference()
+        {
+
+        }
+
+        /// <summary>
+        /// Произведение дробей
+        /// </summary>
+        public Multiply()
+        {
+
+        }
+
+        /// <summary>
+        /// Деление дробей
+        /// </summary>
+        public Division()
+        {
+
+        }
+
+
+    }
+        
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            
+
         }
     }
 }
