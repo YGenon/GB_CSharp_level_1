f2cef959183508c14ebba99a91da7bd5d331f94a
YGenon <genonster@gmail.com>
2019-06-13 17:07:30 +0300

    решил 1 подзадачу

---
+++ ConsoleAppLesson5/Message.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson5
+{
+    static public class Message
+    {
+        static private string newString;
+
+
+        /// <summary>
+        /// Выводит слова сообщения,  которые содержат не более n букв.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="a"></param>
+        static public string OutWords(string s, int a)
+        {
+
+            //newString = s + a;
+            char[] div = { ' ' };
+            string[] splitString = s.Split(div);
+            for (int i = 0; i < splitString.Length; i++)
+            {
+                if (splitString[i].Length <= a) newString += " " + splitString[i];
+
+                //Console.WriteLine(splitString[i]);
+            }
+
+            return newString;
+        }
+
+                        
+       /// <summary>
+       /// Удаляет из сообщения все слова, которые заканчиваются на заданный символ.
+       /// </summary>
+       /// <param name="s"></param>
+       /// <param name="b"></param>
+       /// <returns></returns>
+        static string DellWords(string s, string b)
+        {
+            
+
+            return newString;
+        }
+
+
+        //в) Найти самое длинное слово сообщения.
+        static void LongWords(string s)
+        {
+
+        }
+
+        //г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+        static void StringFromLongWords(string s)
+        {
+
+        }
+    }
+}
