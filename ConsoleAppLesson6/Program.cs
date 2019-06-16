ec78bc43080ce5082bcf3b1d259068dc833eac8b
YGenon <genonster@gmail.com>
2019-06-16 23:10:10 +0300

    добавил файл *.csv для 3 задачи

--- ConsoleAppLesson6/Program.cs
+++ ConsoleAppLesson6/Program.cs
@@ -53,32 +53,37 @@ namespace ConsoleAppLesson6
             int magistr = 0;
 
             List<Student> list = new List<Student>();
-            
+
             // Создаем список студентов 
-            DateTime dt = DateTime . Now;
-            StreamReader sr = new StreamReader("students_6.csv");
+            DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader(@"D:\GIT\ConsoleAppLesson6\students_6.csv");
             while (!sr.EndOfStream)
             {
                 try
                 {
                     string[] s = sr.ReadLine().Split(';'); // Добавляем в список новый экземпляр класса Student 
-                    list . Add ( new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                    list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
                     // Одновременно подсчитываем количество бакалавров и магистров 
-                    if ( int . Parse ( s [ 5 ]) < 5 ) bakalavr ++; else magistr ++;
+                    if (int.Parse(s[5]) < 5) bakalavr++; else magistr++;
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message); Console.WriteLine("Ошибка!ESC - прекратить выполнение программы" );
+                    Console.WriteLine(e.Message); Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
                     // Выход из Main 
-                    if ( Console . ReadKey (). Key == ConsoleKey . Escape ) return;
-}
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                }
             }
+
             sr.Close();
+
             list.Sort(new Comparison<Student>(MyDelegat));
+
             Console.WriteLine("Всего студентов:" + list.Count);
             Console.WriteLine("Магистров:{0}", magistr);
             Console.WriteLine("Бакалавров:{0}", bakalavr);
+
             foreach (var v in list) Console.WriteLine(v.firstName);
+
             Console.WriteLine(DateTime.Now - dt);
             Console.ReadKey();
 
