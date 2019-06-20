bfd75e8c5fd13d8edec14c11ae72825206f55653
YGenon <genonster@gmail.com>
2019-06-20 07:09:22 +0300

    Решил 1 задачу

--- WindowsFormsAppLesson7/Form1.Designer.cs
+++ WindowsFormsAppLesson7/Form1.Designer.cs
@@ -32,11 +32,21 @@
             this.btnCommand2 = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.lblNumber = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.buhfnmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCommand1
             // 
-            this.btnCommand1.Location = new System.Drawing.Point(205, 61);
+            this.btnCommand1.Location = new System.Drawing.Point(184, 44);
             this.btnCommand1.Name = "btnCommand1";
             this.btnCommand1.Size = new System.Drawing.Size(75, 23);
             this.btnCommand1.TabIndex = 0;
@@ -46,7 +56,7 @@
             // 
             // btnCommand2
             // 
-            this.btnCommand2.Location = new System.Drawing.Point(205, 106);
+            this.btnCommand2.Location = new System.Drawing.Point(184, 89);
             this.btnCommand2.Name = "btnCommand2";
             this.btnCommand2.Size = new System.Drawing.Size(75, 23);
             this.btnCommand2.TabIndex = 1;
@@ -56,7 +66,7 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(205, 147);
+            this.btnReset.Location = new System.Drawing.Point(184, 130);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(75, 23);
             this.btnReset.TabIndex = 2;
@@ -67,23 +77,97 @@
             // lblNumber
             // 
             this.lblNumber.AutoSize = true;
-            this.lblNumber.Location = new System.Drawing.Point(56, 70);
+            this.lblNumber.Location = new System.Drawing.Point(6, 22);
             this.lblNumber.Name = "lblNumber";
             this.lblNumber.Size = new System.Drawing.Size(35, 13);
             this.lblNumber.TabIndex = 3;
             this.lblNumber.Text = "label1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(6, 21);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCount.TabIndex = 6;
+            this.lblCount.Text = "label3";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buhfnmToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(281, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // buhfnmToolStripMenuItem
+            // 
+            this.buhfnmToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem});
+            this.buhfnmToolStripMenuItem.Name = "buhfnmToolStripMenuItem";
+            this.buhfnmToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.buhfnmToolStripMenuItem.Text = "Меню";
+            this.buhfnmToolStripMenuItem.Click += new System.EventHandler(this.BuhfnmToolStripMenuItem_Click);
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.игратьToolStripMenuItem.Text = "Играть";
+            this.игратьToolStripMenuItem.Click += new System.EventHandler(this.ИгратьToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblCount);
+            this.groupBox1.Location = new System.Drawing.Point(17, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(145, 43);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Количество ходов";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblNumber);
+            this.groupBox2.Location = new System.Drawing.Point(17, 44);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(145, 41);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Результат";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 258);
-            this.Controls.Add(this.lblNumber);
+            this.ClientSize = new System.Drawing.Size(281, 170);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnCommand2);
             this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,6 +179,13 @@
         private System.Windows.Forms.Button btnCommand2;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem buhfnmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- WindowsFormsAppLesson7/Form1.cs
+++ WindowsFormsAppLesson7/Form1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
+//using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +19,8 @@ namespace WindowsFormsAppLesson7
             btnCommand1.Text = "+1";
             btnCommand2.Text = "x2";
             btnReset.Text = "Сброс";
-            lblNumber.Text = "0";
+            lblNumber.Text = "1";
+            lblCount.Text = "0";
             this.Text = "Удвоитель";
 
         }
@@ -28,16 +29,45 @@ namespace WindowsFormsAppLesson7
         private void BtnCommand1_Click(object sender, EventArgs e)
         {
             lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            lblCount.Text = (int.Parse(lblCount.Text) + 1).ToString();
         }
 
         private void BtnCommand2_Click(object sender, EventArgs e)
         {
             lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            lblCount.Text = (int.Parse(lblCount.Text) + 1).ToString();
         }
 
         private void BtnReset_Click(object sender, EventArgs e)
         {
+            ClearData();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BuhfnmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ИгратьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            ClearData();
+            MessageBox.Show($"Вы должны за минимальное количество ходов получить число -  {r.Next(10,100)}");
+
+        }
+
+        /// <summary>
+        /// Сбрасываем счетчик ходов и результат вычислений
+        /// </summary>
+        private void ClearData()
+        {
             lblNumber.Text = "1";
+            lblCount.Text = "0";
         }
     }
 }
